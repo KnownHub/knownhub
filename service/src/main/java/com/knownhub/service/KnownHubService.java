a67a6fb076ca28615d675373ebc88f2b4167e297
杨圣磊 <shenglei.ysl@alibaba-inc.com>
2017-12-14 14:06:33 +0800

    - update

--- service/src/main/java/com/knownhub/service/KnownHubService.java
+++ service/src/main/java/com/knownhub/service/KnownHubService.java
@@ -2,14 +2,24 @@ package com.knownhub.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author eagel
  * @since 0.0.1
  */
 @SpringBootApplication
+@RestController
 public class KnownHubService {
+    public static final String REQUEST_PREFIX = "service/";
+
     public static void main(String[] args) {
         SpringApplication.run(KnownHubService.class, args);
+    }
+
+    @RequestMapping(REQUEST_PREFIX + "index")
+    public String index() {
+        return "KnownHub";
     }
 }
